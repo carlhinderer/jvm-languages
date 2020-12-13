fbe00c1c6f02439521d08fa62a1ca2836ca6332d
Carl Hinderer <carl.hinderer4@gmail.com>
2020-12-13 13:22:47 -0700

    Added notes on building inheritance hierarchies.

---
+++ java_complete_reference/code/ch08/CallingConstructors.java
@@ -0,0 +1,24 @@
+class A {
+    A() {
+        System.out.println("Inside A's Constructor");
+    }
+}
+
+class B extends A {
+    B() {
+        System.out.println("Inside B's Constructor");
+    }
+}
+
+class C extends B {
+    C() {
+        System.out.println("Inside C's Constructor");
+    }
+}
+
+
+class CallingConstructors {
+    public static void main(String args[]) {
+        C c = new C();
+    }
+}
