319f9b35eb050a5902c2250a9b25a6f03b98684d
Carl Hinderer <carl.hinderer4@gmail.com>
2020-12-17 20:04:12 -0700

    Added notes on chaining exceptions.

---
+++ java_complete_reference/code/ch10/ChainExceptionDemo.java
@@ -0,0 +1,24 @@
+class ChainExceptionDemo {
+    static void demoproc() {
+        // Create an exception
+        NullPointerException e = new NullPointerException("top layer");
+
+        // Add a cause
+        e.initCause(new ArithmeticException("cause"));
+
+        throw e;
+    }
+
+    public static void main(String args[]) {
+        try {
+            demoproc();
+        }
+        catch (NullPointerException e) {
+            // Display top level exception
+            System.out.println("Caught: " + e);
+
+            // Display cause exception
+            System.out.println("Original cause: " + e.getCause());
+        }
+    }
+}
